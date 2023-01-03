7cb48e534744079f46effcbe0e14a63025d1982d
debian vbox <debianvbox@gmail.com>
2023-01-03 16:11:05 -0600

    django #17 pdf file

--- django/mysite/events/urls.py
+++ django/mysite/events/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('delete_venue/<venue_id>',views.delete_venue,name='delete-venue'),
     path('venue_text',views.venue_text,name='venue-text'),
     path('venue_csv',views.venue_csv,name='venue-csv'),
+    path('venue_pdf',views.venue_pdf,name='venue-pdf'),
 
 
 ]
--- django/mysite/events/views.py
+++ django/mysite/events/views.py
@@ -7,7 +7,51 @@ from .models import Event,Venue
 from .forms import VenueForm,EventForm
 from django.http import HttpResponse
 import csv
+#import from pdf file generator
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
 #Generate CSV File Venue List
+
+#Generate pdf FIle
+# For pdf file install pip install reportlab
+
+def venue_pdf(request):
+    #Create Bytestream buffer
+    buf = io.BytesIO()
+    #Create Canvas
+    c = canvas.Canvas(buf,pagesize=letter,bottomup=0)
+    #create text obj
+    textob = c.beginText()
+    textob.setTextOrigin(inch,inch)
+    textob.setFont("Helvetica",14)
+   # lines = [
+   #         "THis line"
+    #        ]
+
+    venues = Venue.objects.all()
+
+    lines = []
+
+    for venue in venues:
+        lines.append(venue.name)
+        lines.append(venue.address)
+        lines.append(venue.zip_code)
+        lines.append(venue.phone)
+        lines.append(venue.web)
+        lines.append(venue.email_address)
+        lines.append(" ")
+
+    for line in lines:
+        textob.textLine(line)
+
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+    return FileResponse(buf,as_attachment=True,filename='venue.pdf')
 
 def venue_csv(request):
     response = HttpResponse(content_type='text/csv')
