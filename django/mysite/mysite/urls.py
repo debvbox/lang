2a3e3b6cbd148a8946d314c428975f55332711b5
debian vbox <debianvbox@gmail.com>
2023-01-07 18:38:39 -0600

    django wednsday #21

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -1,8 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('events.urls')),
+    path('members/',include('django.contrib.auth.urls')),
+    path('members/',include('members.urls')),
 ]
+#Configure Admin Titles
+admin.site.site_header = "My Club Administration Page"
+admin.site.site_title = "Browser title"
+admin.site.index_title = "Welcome To The Admin Area.."
