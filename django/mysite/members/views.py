ff427ead3691eb3c4fc7ba25431d98c21e4c08dd
debian vbox <debianvbox@gmail.com>
2023-01-08 17:37:37 -0600

    django wednsday #25

--- django/mysite/members/views.py
+++ django/mysite/members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import UserCreationForm  
 from django.contrib import messages
+from .forms import RegisterUserForm
 
 def login_user(request):
     if request.method == "POST":
@@ -21,3 +23,19 @@ def logout_user(request):
     logout(request)
     messages.success(request,("You Were Logged Out"))
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"Registration successfully")
+            return redirect('home')
+    else:
+        form = RegisterUserForm()
+
+    return render (request,'authentication/register_user.html',{'form':form})
